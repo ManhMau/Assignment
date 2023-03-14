2b02c3ff3ef28c2aee0888c69af81308d1ee7213
Pham Van Manh <phamvanmanh1608@gmail.com>
2023-03-14 22:36:37 +0700

    Tạo Room.java

---
+++ src/java/model/Room.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author PC
+ */
+public class Room {
+    private int roomNo;
+    private RoomType roomType;
+    private boolean isActive;
+
+    public Room() {
+    }
+
+    public Room(int roomNo, RoomType roomType, boolean isActive) {
+        this.roomNo = roomNo;
+        this.roomType = roomType;
+        this.isActive = isActive;
+    }
+
+    public int getRoomNo() {
+        return roomNo;
+    }
+
+    public void setRoomNo(int roomNo) {
+        this.roomNo = roomNo;
+    }
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(RoomType roomType) {
+        this.roomType = roomType;
+    }
+
+    public boolean isIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+}
