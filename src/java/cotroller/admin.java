813811576a4c5e579b1b92e3d70dcd19c0334514
Pham Van Manh <phamvanmanh1608@gmail.com>
2023-03-20 15:24:57 +0700

    tạo Servlet admin.java

---
+++ src/java/cotroller/admin.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package cotroller;
+
+import dal.BookingDAO;
+import dal.RoomDAO;
+import dal.RoomTypeDAO;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.HashMap;
+import model.Booking;
+import model.Room;
+import model.RoomType;
+
+/**
+ *
+ * @author PC
+ */
+@WebServlet(name="admin", urlPatterns={"/admin"})
+public class admin extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        BookingDAO bookingDAO = new BookingDAO();
+        RoomTypeDAO roomTypeDAO = new RoomTypeDAO();
+        RoomDAO roomDAO = new RoomDAO();
+        ArrayList<RoomType> roomTypeList = roomTypeDAO.getAllRoomType();
+        ArrayList<ArrayList<Room>> list = new ArrayList<>();
+        for (RoomType rt : roomTypeList) {
+            list.add(roomDAO.getRoomByType(rt.getId()));
+        }
+
+        HashMap<Integer, Booking> bs = bookingDAO.getBookingList();
+        request.setAttribute("roomTypeList", roomTypeList);
+        request.setAttribute("list", list);
+        request.setAttribute("booking", bs);
+        request.getRequestDispatcher("admin.jsp").forward(request, response);
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
